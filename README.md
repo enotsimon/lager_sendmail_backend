08c620ce6a85ad8b5b6f65a03612cc3ba95b49bb
Иван Скоробогатько <skorobogatko@superjob.ru>
2016-03-29 10:20:52 +0300

    move code from another repo

---
+++ README.md
@@ -0,0 +1,42 @@
+# lager_sendmail_backend
+
+## overwiev
+
+backend for erlang basho lager (https://github.com/basho/lager) that sends letters thru sendmail
+
+
+## install using rebar
+
+add
+
+`{lager_sendmail_backend, ".*", {git, "https://github.com/skorobogatko/lager_sendmail_backend.git", {branch, "master"}}}`
+
+to your `rebar.config` file in your erlang app
+
+
+## usage
+
+add to your app config files something like this
+
+```
+{lager, [
+    {handlers, [
+        {lager_sendmail_backend, [
+            {level, error},
+            {from, "lager_sendmail_backend <from@example.com>"},
+            {to, ["errors@example.com"]},
+            {subject, "erlang errors in my app"},
+            {aggregate_interval, 60000}, % aggregate messages every 1 min. thats default
+            {msg_limit, 20}, % messages per letter. thats default
+            {sendmail_cmd, "/usr/sbin/sendmail -t"} % cmd to send email. thats default. use "cat > bla" for debug
+        ]},
+        {lager_sendmail_backend, [
+            {level, warning},
+            {from, "lager_sendmail_backend <from@example.com>"},
+            {to, ["warnings@example.com"]},
+            {subject, "erlang warnings in my app"}
+        ]}
+    ]}
+]}
+```
+unfortunately, you cannot use one instance of backend for both warnings and errors for now...
---
+++ src/lager_sendmail_backend.erl
@@ -0,0 +1,134 @@
+-module(lager_sendmail_backend).
+-behaviour(gen_event).
+-include_lib("lager/include/lager.hrl").
+
+-export([init/1, handle_call/2, handle_event/2, handle_info/2, terminate/2, code_change/3]).
+
+-define(AGGREGATE_TIME, 60000).
+-define(MSG_LIMIT, 20).
+
+-record(state,{
+		uid :: {integer(), integer(), integer()},
+		from :: binary(),
+		to :: [binary()],
+		subject :: binary(),
+		level :: integer(),
+		msg_limit :: integer(),
+		timer = void :: reference()|'void',
+		messages = [] :: list(),
+		msg_count = 0 :: integer()
+	}).
+
+init(Config) ->
+	{ok, #state{
+			uid = os:timestamp(),
+			from = list_to_binary(proplists:get_value(from, Config)),
+			to = [list_to_binary(E) || E <- proplists:get_value(to, Config)],
+			subject = list_to_binary(proplists:get_value(subject, Config)),
+			level = lager_util:level_to_num(proplists:get_value(level, Config)),
+			msg_limit = proplists:get_value(msg_limit, Config, ?MSG_LIMIT)
+		}}.
+
+
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+handle_call(get_loglevel, #state{level = LogLevel} = State) ->
+	{ok, LogLevel, State};
+
+handle_call({set_loglevel, LogLevel}, State) ->
+	case lists:member(LogLevel, ?LEVELS) of
+		true ->
+			{ok, ok, State#state{level = lager_util:level_to_num(LogLevel)}};
+		_ ->
+			{ok, {error, bad_log_level}, State}
+	end;
+
+handle_call(V, State) -> {stop, {unexpected_call, V}, State}.
+
+
+
+
+handle_event({log, Message}, State) ->
+	SeverityAsInt = lager_msg:severity_as_int(Message),
+	NewState = handle_log(Message, SeverityAsInt, State),
+	{ok, NewState};
+
+handle_event(V, State) ->
+	{stop, {unexpected_event, V}, State}.
+
+
+% message from port after closing
+handle_info({'EXIT', _Port, _Reason}, State) ->
+	{ok, State};
+
+handle_info({flush_aggregated, MUid}, #state{uid = Uid} = State) when MUid =:= Uid ->
+	#state{msg_count = MsgCount, msg_limit = MsgLimit} = State,
+	erlang:cancel_timer(State#state.timer),
+	DDD = case MsgCount > MsgLimit of
+		true ->
+			CountOfMsgs = unicode:characters_to_binary(io_lib:format("~p of ~p messages:\n\n", [MsgLimit, MsgCount])),
+			[CountOfMsgs | lists:reverse(State#state.messages)];
+		false ->
+			lists:reverse(State#state.messages)
+	end,
+	Data = << << LetterText/binary, "\n\n" >> || LetterText <- DDD >>,
+	To = << <<" ", E/binary>> || E <- State#state.to >>,
+	send(To, State#state.from, State#state.subject, Data),
+	{ok, State#state{timer = void, msg_count = 0, messages = []}};
+
+% not our 'flush_aggregated' message
+handle_info({flush_aggregated, _MUid}, State) ->
+	{ok, State};
+
+% ignore log rotate events
+handle_info({rotate, _LogFile}, State) ->
+	{ok, State};
+
+handle_info(V, State) -> {stop, {unexpected_info, V}, State}.
+
+
+
+%%
+%% PRIVATE
+%%
+handle_log(_Message, SeverityAsInt, #state{level = Level, msg_count = MsgCount, msg_limit = MsgLimit} = State) when (SeverityAsInt =:= Level) and (MsgCount > MsgLimit) ->
+	State#state{msg_count = MsgCount+1};
+
+handle_log(Message, SeverityAsInt, #state{level = Level} = State) when SeverityAsInt =:= Level ->
+	Msg = lager_msg:message(Message),
+	%Timestamp = lager_msg:timestamp(Message),
+	{Date, Time} = lager_msg:datetime(Message),
+	Severity = lager_msg:severity(Message),
+	Metadata = lager_msg:metadata(Message),
+	Format = "~s ~s ~p\nmessage: ~ts\nmeta: ~p",
+	LetterText = unicode:characters_to_binary(io_lib:format(Format, [Date, Time, Severity, Msg, Metadata])),
+	#state{messages = Messages, msg_count = MsgCount, timer = Timer, uid = Uid} = State,
+	State#state{timer = install_new_timer(Timer, Uid), msg_count = MsgCount+1, messages = [LetterText | Messages]};
+
+handle_log(_Message, _SeverityAsInt, State) ->
+	State.
+
+
+install_new_timer(void, Uid) -> erlang:send_after(?AGGREGATE_TIME, self(), {flush_aggregated, Uid});
+install_new_timer(Timer, _Uid) -> Timer. 
+
+
+
+send(To, From, Subject, Message) ->
+	Letter = <<
+		"To: ", To/binary, "\r\n",
+		"MIME-Version: 1.0\r\n",
+		"Content-type: text/plain; charset=utf-8\r\n",
+		"Content-transfer-encoding: binary\r\n",
+		"From: ", From/binary,"\r\n",
+		"Subject: =?utf-8?B?", (base64:encode(Subject))/binary, "?=\r\n",
+		"\r\n",
+		Message/binary,
+		"\r\n.\r\n"
+	>>,
+	Port = open_port({spawn, "/usr/sbin/sendmail -t"}, [binary]),
+	Port ! {self(), {command, Letter}},
+	port_close(Port).
+
+
